d91187e442edd8fbbbd500a5e4591dd323d63803
koki yoda <kuk.3q.anm@i.softbank.jp>
2023-10-08 07:17:54 +0900

    first commit

---
+++ src/main/java/kukanm/example/chat_werewolf/Chat_werewolf.java
@@ -0,0 +1,17 @@
+package kukanm.example.chat_werewolf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chat_werewolf extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
